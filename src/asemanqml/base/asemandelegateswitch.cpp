aec1e47f02cf7876649909f34b98b15782e64b83
Bardia Daneshvar <bardia.daneshvar@gmail.com>
2020-03-28 01:30:09 +0430

    Add icon to readme

---
+++ src/asemanqml/base/asemandelegateswitch.cpp
@@ -0,0 +1,35 @@
+/*
+    Copyright (C) 2019 Aseman Team
+    http://aseman.io
+
+    This project is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This project is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "asemandelegateswitch.h"
+
+class AsemanDelegateSwitch::Private
+{
+public:
+};
+
+AsemanDelegateSwitch::AsemanDelegateSwitch(QQuickItem *parent) :
+    QQuickItem(parent)
+{
+    p = new Private;
+}
+
+AsemanDelegateSwitch::~AsemanDelegateSwitch()
+{
+    delete p;
+}
